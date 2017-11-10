774e5d207e00b973cd4efb71f372709d138ed248
Bohee Park <boheepark@yahoo.com>
2017-11-09 21:45:29 -0500

    commented out stuff

--- asdf/config.py
+++ asdf/config.py
@@ -1,9 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.db")
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.db")
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Config:
     DEBUG = False
