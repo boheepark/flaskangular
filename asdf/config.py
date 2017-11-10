34a3f28903b1abfd6d0c7a3dfa218afa0ec73b88
unknown <boheepark@yahoo.com>
2017-11-10 00:24:00 -0500

    env variable settings

--- asdf/config.py
+++ asdf/config.py
@@ -28,7 +28,7 @@ class StagingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 app_config = {
     "development": DevelopmentConfig,
