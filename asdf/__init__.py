2bbfc25d46e843f96c3c988f436bcb39314a9a7a
Bohee Park <boheepark@yahoo.com>
2017-11-15 16:35:18 -0500

    created create_app function

--- asdf/__init__.py
+++ asdf/__init__.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-app = Flask(__name__)
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
-db = SQLAlchemy(app)
-if app.config["DEBUG"]:
-    from werkzeug import SharedDataMiddleware
-    import os
-    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-        "/": os.path.join(os.path.dirname(__file__), "static")
-    })
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
-from asdf.models import user, trade
-from asdf.views import base, auth, user, trade
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
+    db.init_app(app)
+    if app.config["DEBUG"]:
+        from werkzeug import SharedDataMiddleware
+        import os
+        app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+            "/": os.path.join(os.path.dirname(__file__), "static")
+        })
+    from flask_bcrypt import Bcrypt
+    bcrypt = Bcrypt(app)
+    from asdf.api.views.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from asdf.api.models import user, trade
+    from asdf.api.views import base, auth, user, trade
+
+    return app
