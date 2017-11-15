7cc412a4754d7b5c67ea9f647544080aee8d34e0
Bohee Park <boheepark@yahoo.com>
2017-11-15 17:12:48 -0500

    cleanup

--- asdf/tests/test_users.py
+++ asdf/tests/test_users.py
@@ -3,10 +3,8 @@ import json
 from asdf.tests.base import BaseTestCase
 
 class TestUserService(BaseTestCase):
-    """Tests for the Users Service."""
 
     def test_users(self):
-        """Ensure the /api/user/unames route behaves correctly."""
         res = self.client.get('/api/user/unames')
         data = json.loads(res.data.decode())
         self.assertEqual(res.status_code, 200)
