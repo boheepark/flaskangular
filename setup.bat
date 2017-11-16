2abd867aaec65696f0a3a2dc8582570b2c68c748
Bohee Park <boheepark@yahoo.com>
2017-11-16 01:26:39 -0500

    my first bat file

---
+++ setup.bat
@@ -0,0 +1,4 @@
+venv\Scripts\activate
+SET APP_SETTINGS="asdf.config.DevelopmentConfig"
+SET DATABASE_URL="postgres://postgres:postgres@asdf-db:5432/asdf_dev"
+SET DATABASE_TEST_URL="postgres://postgres:postgres@asdf-db:5432/asdf_test"
