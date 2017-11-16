2fae3da2e76583b6840da7f2bf1e0286e17a2d0d
Bohee Park <boheepark@yahoo.com>
2017-11-15 19:24:02 -0500

    added basic signup tests

--- asdf/tests/test_auth.py
+++ asdf/tests/test_auth.py
@@ -28,3 +28,59 @@ class TestAuthService(BaseTestCase):
             data = json.loads(res.data.decode())
             self.assertEqual(res.status_code, 200)
             self.assertIn("uname", data["data"])
+
+    def test_signup_empty_json(self):
+        with self.client:
+            res = self.client.post("/auth/signup", data=json.dumps(dict()), content_type="application/json")
+            data = json.loads(res.data.decode())
+            self.assertEqual(res.status_code, 400)
+            self.assertIn("fail", data["status"])
+
+    def test_signup_missing_fields(self):
+        with self.client:
+            res = self.client.post("/auth/signup", data=json.dumps(dict(
+                uname="uname",
+                name="name",
+                email="email@test.com",
+                pw="Left3.5Right3.75"
+            )),
+            content_type="application/json")
+            data = json.loads(res.data.decode())
+            self.assertEqual(res.status_code, 400)
+            self.assertIn("fail", data["status"])
+
+    def test_signup_duplicate_user(self):
+        with self.client:
+            self.client.post("/auth/signup", data=json.dumps(dict(
+                uname="uname",
+                name="name",
+                email="email@test.com",
+                pw="Left3.5Right3.75",
+                checking=100000,
+                trading=100000,
+                gender="male",
+                phone="1234567890",
+                addr="1234 Main St.",
+                town="NYC",
+                state="NY",
+                zip="12345"
+            )),
+            content_type="application/json")
+            res = self.client.post("/auth/signup", data=json.dumps(dict(
+                uname="uname",
+                name="name",
+                email="email@test.com",
+                pw="Left3.5Right3.75",
+                checking=100000,
+                trading=100000,
+                gender="male",
+                phone="1234567890",
+                addr="1234 Main St.",
+                town="NYC",
+                state="NY",
+                zip="12345"
+            )),
+            content_type="application/json")
+            data = json.loads(res.data.decode())
+            self.assertEqual(res.status_code, 400)
+            self.assertIn("fail", data["status"])
