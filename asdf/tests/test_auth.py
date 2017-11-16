6d3ddbf1748e1204096f6f6d853de2344a34de58
Bohee Park <boheepark@yahoo.com>
2017-11-15 21:07:07 -0500

    cleanup and added duplicate email test

--- asdf/tests/test_auth.py
+++ asdf/tests/test_auth.py
@@ -7,18 +7,18 @@ class TestAuthService(BaseTestCase):
     def test_signup(self):
         with self.client:
             res = self.client.post("/auth/signup", data=json.dumps(dict(
-                uname="uname",
-                name="name",
+                uname="test",
+                name="test",
                 email="email@test.com",
-                pw="Left3.5Right3.75",
+                pw="TestPa$$3121",
                 checking=100000,
                 trading=100000,
                 gender="male",
                 phone="1234567890",
                 addr="1234 Main St.",
-                town="NYC",
-                state="NY",
-                zip="12345"
+                town="Fort Lee",
+                state="NJ",
+                zip="07024"
             )),
             content_type="application/json")
             data = json.loads(res.data.decode())
@@ -27,7 +27,7 @@ class TestAuthService(BaseTestCase):
             res = self.client.get("/api/user/unames")
             data = json.loads(res.data.decode())
             self.assertEqual(res.status_code, 200)
-            self.assertIn("uname", data["data"])
+            self.assertIn("test", data["data"])
 
     def test_signup_empty(self):
         with self.client:
@@ -40,10 +40,10 @@ class TestAuthService(BaseTestCase):
     def test_signup_incomplete(self):
         with self.client:
             res = self.client.post("/auth/signup", data=json.dumps(dict(
-                uname="uname",
-                name="name",
+                uname="test",
+                name="test",
                 email="email@test.com",
-                pw="Left3.5Right3.75"
+                pw="TestPa$$3121"
             )),
             content_type="application/json")
             data = json.loads(res.data.decode())
@@ -51,38 +51,74 @@ class TestAuthService(BaseTestCase):
             self.assertIn("Incomplete payload.", data["message"])
             self.assertIn("fail", data["status"])
 
-    def test_signup_duplicate_user(self):
+    def test_signup_duplicate_uname(self):
         with self.client:
             self.client.post("/auth/signup", data=json.dumps(dict(
-                uname="uname",
-                name="name",
+                uname="test",
+                name="test",
                 email="email@test.com",
-                pw="Left3.5Right3.75",
+                pw="TestPa$$3121",
                 checking=100000,
                 trading=100000,
                 gender="male",
                 phone="1234567890",
                 addr="1234 Main St.",
-                town="NYC",
-                state="NY",
-                zip="12345"
+                town="Fort Lee",
+                state="NJ",
+                zip="07024"
             )),
             content_type="application/json")
             res = self.client.post("/auth/signup", data=json.dumps(dict(
-                uname="uname",
-                name="name",
-                email="email@test.com",
-                pw="Left3.5Right3.75",
+                uname="test",
+                name="test2",
+                email="email2@test.com",
+                pw="TestPa$$3121",
                 checking=100000,
                 trading=100000,
                 gender="male",
                 phone="1234567890",
                 addr="1234 Main St.",
-                town="NYC",
-                state="NY",
-                zip="12345"
+                town="Fort Lee",
+                state="NJ",
+                zip="07024"
             )),
             content_type="application/json")
             data = json.loads(res.data.decode())
             self.assertEqual(res.status_code, 400)
             self.assertIn("fail", data["status"])
+
+        def test_signup_duplicate_email(self):
+            with self.client:
+                self.client.post("/auth/signup", data=json.dumps(dict(
+                    uname="test",
+                    name="test",
+                    email="email@test.com",
+                    pw="TestPa$$3121",
+                    checking=100000,
+                    trading=100000,
+                    gender="male",
+                    phone="1234567890",
+                    addr="1234 Main St.",
+                    town="Fort Lee",
+                    state="NJ",
+                    zip="07024"
+                )),
+                content_type="application/json")
+                res = self.client.post("/auth/signup", data=json.dumps(dict(
+                    uname="test2",
+                    name="test2",
+                    email="email@test.com",
+                    pw="TestPa$$3121",
+                    checking=100000,
+                    trading=100000,
+                    gender="male",
+                    phone="1234567890",
+                    addr="1234 Main St.",
+                    town="Fort Lee",
+                    state="NJ",
+                    zip="07024"
+                )),
+                content_type="application/json")
+                data = json.loads(res.data.decode())
+                self.assertEqual(res.status_code, 400)
+                self.assertIn("fail", data["status"])
