a32d1d966f515ed6a31570311d361b4939ce1d9f
Bohee Park <boheepark@yahoo.com>
2017-11-15 21:14:20 -0500

    added signin test

--- asdf/tests/test_auth.py
+++ asdf/tests/test_auth.py
@@ -122,3 +122,29 @@ class TestAuthService(BaseTestCase):
                 data = json.loads(res.data.decode())
                 self.assertEqual(res.status_code, 400)
                 self.assertIn("fail", data["status"])
+
+        def test_signin(self):
+            with self.client:
+                self.client.post("/auth/signup", data=json.dumps(dict(
+                    uname="test",
+                    name="test",
+                    email="email@test.com",
+                    pw="TestPa$$3121",
+                    checking=100000,
+                    trading=100000,
+                    gender="male",
+                    phone="1234567890",
+                    addr="1234 Main St.",
+                    town="Fort Lee",
+                    state="NJ",
+                    zip="07024"
+                )),
+                content_type="application/json")
+                res = self.client.post("/auth/signin", data=json.dumps(dict(
+                    uname="test",
+                    pw="TestPa$$3121"
+                )),
+                content_type="application/json")
+                data = json.loads(res.data.decode())
+                self.assertEqual(res.status_code, 200)
+                self.assertIn("success", data["status"])
