f4c5289f6aa4b2a84baecca274f91b3627ccf2a1
Bohee Park <boheepark@yahoo.com>
2017-11-16 02:13:03 -0500

    hmm

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-venv\Scripts\activate
-SET APP_SETTINGS="asdf.config.DevelopmentConfig"
-SET DATABASE_URL="postgres://postgres:postgres@asdf-db:5432/asdf_dev"
-SET DATABASE_TEST_URL="postgres://postgres:postgres@asdf-db:5432/asdf_test"
+REM venv\Scripts\activate
+SET APP_SETTINGS=asdf.config.DevelopmentConfig
+SET DATABASE_URL=postgres://postgres:postgres@asdf-db:5432/asdf_dev
+SET DATABASE_TEST_URL=postgres://postgres:postgres@asdf-db:5432/asdf_test
